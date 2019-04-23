cfe8ca1a2a5e7c4a422e3c7b4f38ade8fbec20a5
Goma0925 <amonbali0925@gmail.com>
2019-04-22 22:57:34 -0400

    4/22 Completed

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ svgScreen = {
 svgMargin = {
   top: 30,
   bottom:30,
-  left: 50,
-  right:50
+  left: 0,
+  right: 80
 }
 
 var boxSize = 0; //Initialized in initBoxSize()
@@ -17,15 +17,30 @@ var boxSize = 0; //Initialized in initBoxSize()
 var xScale = d3.scaleLinear();
 var yScale = d3.scaleLinear();
 
+var startColor = "#ffffff";
+var endColor = "#3498DB"
+var colorMap = function(val){
+  colorScale = d3.scaleLinear()
+              .domain([0, 1])
+              .range([startColor, endColor]);
+  if (val === 1){
+    return "black";
+  }
+  else {
+    return colorScale(val);
+  }
+};
+
 //-----------------------------------------------------------------------
 
 
 //Initializers-----------------------------------------------------------
 var initScales = function(numOfStudents){
   xScale.domain([0, numOfStudents])
-        .range([svgMargin.left + (boxSize/2), svgScreen.size - boxSize/2 - svgMargin.right])
+        .range([svgMargin.left + boxSize, svgScreen.size - svgMargin.right - boxSize]);
   yScale.domain([0, numOfStudents])
-        .range([svgMargin.top + (boxSize/2), svgScreen.size - boxSize])
+        .range([svgMargin.left + boxSize, svgScreen.size - svgMargin.right - boxSize]);
+
 };
 
 var initBoxSize = function(numOfStudents){
@@ -85,8 +100,10 @@ var drawGraph = function(dataArr, picSrcArr){
   var graphSvg = d3.select("#main-graph")
                 .append("svg")
                 .attr("class", "svg-graph")
+                .attr("width", svgScreen.size)
+                .attr("height", svgScreen.size);
 
-  var studentPics = graphSvg
+  var studentPicRow = graphSvg
                 .append("g")
                 .attr("class", "studetn-pics")
                 .selectAll("image")
@@ -94,12 +111,100 @@ var drawGraph = function(dataArr, picSrcArr){
                 .enter()
                 .append("image")
                 .attr("xlink:href", function(picSrc){return "/student_images/" + picSrc})
-                .attr("x", function(picSrc, i){return xScale(i)})
+                .attr("x", function(picSrc, i){return xScale(i+1)})
                 .attr("y", function(){return yScale(0)})
                 .attr("width", boxSize)
                 .attr("height", boxSize);
                 console.log("picSize:", boxSize);
 
+  var studentPicCol = graphSvg
+                .append("g")
+                .attr("class", "studetn-pics")
+                .selectAll("image")
+                .data(picSrcArr)
+                .enter()
+                .append("image")
+                .attr("xlink:href", function(picSrc){return "/student_images/" + picSrc})
+                .attr("x", xScale(0))
+                .attr("y", function(picSrc, i){return yScale(i+1)})
+                .attr("width", boxSize)
+                .attr("height", boxSize);
+                console.log("picSize:", boxSize);
+
+
+  var dataRect = graphSvg.append("g")
+                          .attr("class", "data-rect")
+                          .selectAll("rect")
+                          .data(dataArr)
+                          .enter()
+                          .append("rect")
+                          .attr("x", function(corrObj){return xScale(corrObj.x + 1)})
+                          .attr("y", function(corrObj){return yScale(corrObj.y + 1)})
+                          .attr("width", boxSize)
+                          .attr("height", boxSize)
+                          .attr("stroke", "#555556")
+                          .attr("stroek-width", 1)
+                          .attr("fill", function(corrObj){return colorMap(corrObj.r)})
+//------------------------
+
+var legendWidth = 80;
+var legendHeight = 300;
+
+var legendScale = d3.scaleLinear()
+                    .domain([1, 0])
+                    .range([0, legendHeight])
+
+var axisFunc = d3.axisRight()
+                .scale(legendScale);
+
+var legend = graphSvg
+.append("defs")
+.append("svg:linearGradient")
+.attr("id", "gradient")
+.attr("x1", "100%")
+.attr("y1", "0%")
+.attr("x2", "100%")
+.attr("y2", "100%")
+.attr("spreadMethod", "pad");
+
+legend
+.append("stop")
+.attr("offset", "0%")
+.attr("stop-color", endColor)
+.attr("stop-opacity", 1);
+
+legend
+.append("stop")
+.attr("offset", "100%")
+.attr("stop-color", startColor)
+.attr("stop-opacity", 1);
+
+graphSvg.append("rect")
+.attr("width", legendWidth/2-13)
+.attr("height", legendHeight)
+.style("fill", "url(#gradient)")
+.attr("stroke", "#555556")
+.attr("transform", "translate(" +  (svgScreen.size - 50) + "," + boxSize + ")");
+
+legenAxis = graphSvg.append("g")
+            .attr("class", "legen-axis")
+            .call(axisFunc)
+            .attr("transform", "translate(" +  (svgScreen.size - 50 + legendWidth/2-13) + "," + boxSize + ")");;
+
+
+
+
+
+
+  //----------------------
+  // var studentPics = graphSvg
+  //                   .append("circle")
+  //                   .attr("cx", xScale(23))
+  //                   .attr("cy", 10)
+  //                   .attr("r", 3)
+  //                   .attr("fill", red)
+
+console.log("x domai:", xScale(23));
 };
 
 dataP.then(function(data){
